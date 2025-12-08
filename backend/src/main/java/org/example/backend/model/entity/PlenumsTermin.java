0390d5c43b74cdb708a595252b7a9749d9add9a0
chris s <test@test.com>
2025-12-08 13:41:05 +0100

    changes spelling

--- backend/src/main/java/org/example/backend/model/entity/PlenumsTermin.java
+++ backend/src/main/java/org/example/backend/model/entity/PlenumsTermin.java
@@ -4,7 +4,7 @@ import lombok.With;
 import org.example.backend.utils.enums.Subgroup;
 
 @With
-public record plenumsTermin(
+public record PlenumsTermin(
         String id,
         String date,
         Subgroup group,
