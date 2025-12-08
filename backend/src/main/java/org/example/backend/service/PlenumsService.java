30935fa1a23a86af40b7b114f7205a09d83346af
chris s <test@test.com>
2025-12-08 13:41:26 +0100

    created plenumsrepo and plenumsservice

---
+++ backend/src/main/java/org/example/backend/service/PlenumsService.java
@@ -0,0 +1,24 @@
+package org.example.backend.service;
+
+import org.example.backend.model.entity.PlenumsTermin;
+import org.example.backend.respository.PlenumsRepository;
+import org.example.backend.utils.enums.Subgroup;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlenumsService {
+
+    private final PlenumsRepository plenumsRepo;
+
+    public PlenumsService(PlenumsRepository plenumsRepo){
+        this.plenumsRepo = plenumsRepo;
+        PlenumsTermin testPlenumstermin = new PlenumsTermin("1", "1.1.2026", Subgroup.WERKSTATT, new String[]{"erster TOP", "zweiter TOP", "dritter TOP"});
+        plenumsRepo.save(testPlenumstermin);
+    }
+
+    public List<PlenumsTermin> getAll(){
+        return plenumsRepo.findAll();
+    }
+}
