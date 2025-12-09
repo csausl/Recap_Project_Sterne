8afb547ff46bc7e3c9d5b134df722e81071cde67
chris s <test@test.com>
2025-12-09 19:41:00 +0100

    moved content to Testpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,14 @@
 import './App.css'
-import axios from "axios";
-import {useState} from "react";
+import Testpage from "./Pages/Testpage.tsx";
 
-type Subgroup ="WERKSTATT" | "FEMINISTA" | "RSG"|"RSL"|undefined
-
-type Plenumstermin={
-    id: string,
-    date: string,
-    group: Subgroup
-    tops: string[]
-}
 
 function App() {
 
-    const [plena, setPlena] = useState<Plenumstermin[]>([]);
 
-    function getAllPlena(){
-        axios.get("/api/plena").then((response) => {
-            console.log(response.data);
-            setPlena(response.data);
-        })
-
-    }
-    function clear(){
-        setPlena([])
-    }
 
   return (
     <>
-      <h1>hallo</h1>
-        <button onClick={getAllPlena}>Get All!</button>
-        <button onClick={clear}>clear!</button>
-        <div>
-            {plena.map((plenum) => (
-                <>
-                    <p>id: {plenum.id}</p>
-                    <p>date: {plenum.date}</p>
-                    <p>group: {plenum.group}</p>
-                    <ul>Tops:
-                        {plenum.tops.map(top => (<li>{top}</li>))}
-                    </ul>
-                </>
-      ))}
-        </div>
+        <Testpage/>
     </>
   )
 }
