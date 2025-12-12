7f46ead2afa57a881e87d7b1496f4e5ee98a0342
chris s <test@test.com>
2025-12-12 12:49:25 +0100

    moved creating plenumstermin to component

---
+++ frontend/src/Components/AddPlenumsTerminCard.tsx
@@ -0,0 +1,86 @@
+import {type ChangeEvent, type FormEvent, useEffect, useState} from "react";
+import {type PlenumsTerminDto, type Subgroup, subgroups} from "../Types/Types.ts";
+import axios from "axios";
+
+export default function AddPlenumsTerminCard() {
+    const [plenumDate, setPlenumDate] = useState<string>("");
+    const [plenumGroup, setPlenumGroup] = useState<Subgroup>("ALLE");
+    const [plenumFirstTop, setPlenumFirstTop] = useState<string>("");
+    const [plenumSecondTop, setPlenumSecondTop] = useState<string>("");
+    const [plenumThirdTop, setPlenumThirdTop] = useState<string>("");
+    const [plenumsTerminDto, setPlenumsTerminDto] = useState<PlenumsTerminDto>()
+
+    function handleSubmit(event:FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        //console.log(plenumsTerminDto, plenumDate, plenumGroup, plenumFirstTop, plenumSecondTop);
+        setPlenumsTerminDto({date: plenumDate, group: plenumGroup, tops: [plenumFirstTop,plenumSecondTop,plenumThirdTop]});
+        //setPlenumsTerminDto({date: "Test", group: "FEMINISTA", tops: ["top1","Top2","Top3"]});
+       // console.log(plenumsTerminDto);
+    }
+
+    const handleChange = (event:ChangeEvent<HTMLSelectElement>) => {
+        setPlenumGroup(event.target.value as Subgroup);
+    };
+
+    function resetForm() {
+        setPlenumDate("");
+        setPlenumGroup("ALLE");
+        setPlenumFirstTop("");
+        setPlenumSecondTop("");
+        setPlenumThirdTop("");
+    }
+
+    function addNewPlenumstermin(){
+        axios.post("/api/plena",
+            plenumsTerminDto)
+            .then(response => {console.log(response.data);})
+            .then(resetForm)
+    }
+
+    useEffect(() => {
+        addNewPlenumstermin();
+        console.log("raw: " ,plenumDate, plenumGroup, plenumFirstTop, plenumSecondTop);
+        console.log(plenumsTerminDto);
+    }, [plenumsTerminDto]);
+    return (
+        <>
+            <form className={"plenumsForm"} onSubmit={handleSubmit}>
+                <label>Date:<input
+                    name={"Date"}
+                    onChange={(e) => setPlenumDate(e.target.value)}
+                    placeholder={"Enter Date"}
+                    value={plenumDate}/>
+                </label>
+                <label>Top 1: <input
+                    name={"Top 1"}
+                    onChange={(e) => setPlenumFirstTop(e.target.value)}
+                    placeholder={"Enter Top 1"}
+                    value={plenumFirstTop}/>
+                </label>
+                <label>Top 2: <input
+                    name={"Top 2"}
+                    onChange={(e) => setPlenumSecondTop(e.target.value)}
+                    placeholder={"Enter Top 2"}
+                    value={plenumSecondTop}/>
+                </label>
+                <label>Top 3: <input
+                    name={"Top 3"}
+                    onChange={(e) => setPlenumThirdTop(e.target.value)}
+                    placeholder={"Enter Top 3"}
+                    value={plenumThirdTop}/>
+                </label>
+                <label>Gruppe:<select
+                    value={plenumGroup}
+                    //onChange={handleChange}>
+                    onChange={handleChange}>
+                    {subgroups.map((category) => (
+                        <option value={category.value} key={category.value}>{category.label}</option>
+                    ))}
+                </select>
+                </label>
+                <button type="submit">Submit</button>
+                <button type="reset" onClick={resetForm}>Reset</button>
+            </form>
+        </>
+    )
+}
--- frontend/src/Pages/Testpage.tsx
+++ frontend/src/Pages/Testpage.tsx
@@ -1,58 +1,24 @@
-import {type ChangeEvent, type FormEvent, useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
-import {type PlenumsTermin, type PlenumsTerminDto, type Subgroup, subgroups} from "../Types/Types.ts";
+import {type PlenumsTermin} from "../Types/Types.ts";
 import PlenumsCard from "../Components/PlenumsCard.tsx";
+
+import AddPlenumsTerminCard from "../Components/AddPlenumsTerminCard.tsx";
 
 
 export default function Testpage() {
-    const [plenumsTerminDto, setPlenumsTerminDto] = useState<PlenumsTerminDto>()
-    const [plenumDate, setPlenumDate] = useState<string>("");
-    const [plenumGroup, setPlenumGroup] = useState<Subgroup>("ALLE");
-    const [plenumFirstTop, setPlenumFirstTop] = useState<string>("");
-    const [plenumSecondTop, setPlenumSecondTop] = useState<string>("");
-    const [plenumThirdTop, setPlenumThirdTop] = useState<string>("");
+
     const [plena, setPlena] = useState<PlenumsTermin[]>([]);
+
+    function clear(){
+        setPlena([])
+    }
 
     function getAllPlena(){
         axios.get("/api/plena").then((response) => {
             console.log(response.data);
             setPlena(response.data);
         })
-
-    }
-    function clear(){
-        setPlena([])
-    }
-
-    function handleSubmit(event:FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        console.log(plenumsTerminDto, plenumDate, plenumGroup, plenumFirstTop, plenumSecondTop);
-        setPlenumsTerminDto({date: plenumDate, group: plenumGroup, tops: [plenumFirstTop,plenumSecondTop,plenumThirdTop]});
-        console.log(plenumsTerminDto);
-        addNewPlenumstermin();
-    }
-
-    const handleChange = (event:ChangeEvent<HTMLSelectElement>) => {
-        setPlenumGroup(event.target.value as Subgroup);
-        console.log(plenumGroup);
-    };
-
-    function resetForm() {
-        setPlenumDate("");
-        setPlenumGroup("ALLE");
-        setPlenumFirstTop("");
-        setPlenumSecondTop("");
-        setPlenumThirdTop("");
-    }
-
-    function addNewPlenumstermin(){
-        if(plenumsTerminDto){
-            axios.post("/api/plena",
-                plenumsTerminDto)
-                .then(response => {console.log(response.data);})
-                .then(getAllPlena)
-                .then(resetForm)
-        }
 
     }
 
@@ -71,42 +37,7 @@ export default function Testpage() {
         </div>
         <div className={"mainContainer"}>
             <div className={"leftContainer"}>
-                <form className={"plenumsForm"} onSubmit={handleSubmit}>
-                    <label>Date:<input
-                        name={"Date"}
-                        onChange={(e) => setPlenumDate(e.target.value)}
-                        placeholder={"Enter Date"}
-                        value={plenumDate}/>
-                    </label>
-                    <label>Top 1: <input
-                        name={"Top 1"}
-                        onChange={(e) => setPlenumFirstTop(e.target.value)}
-                        placeholder={"Enter Top 1"}
-                        value={plenumFirstTop}/>
-                    </label>
-                    <label>Top 2: <input
-                        name={"Top 2"}
-                        onChange={(e) => setPlenumSecondTop(e.target.value)}
-                        placeholder={"Enter Top 2"}
-                        value={plenumSecondTop}/>
-                    </label>
-                    <label>Top 3: <input
-                        name={"Top 3"}
-                        onChange={(e) => setPlenumThirdTop(e.target.value)}
-                        placeholder={"Enter Top 3"}
-                        value={plenumThirdTop}/>
-                    </label>
-                    <label>Gruppe:<select
-                        value={plenumGroup}
-                        onChange={handleChange}>
-                        {subgroups.map((category) => (
-                            <option value={category.value} key={category.value}>{category.label}</option>
-                        ))}
-                    </select>
-                    </label>
-                    <button type="submit">Submit</button>
-                    <button type="reset" onClick={resetForm}>Reset</button>
-                </form>
+                <AddPlenumsTerminCard/>
 
             </div>
             <div className={"rightContainer"}>
