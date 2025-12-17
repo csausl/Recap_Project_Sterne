ea82c93a2eedc72c02595ce402b9338710c30bf7
chris s <test@test.com>
2025-12-17 21:01:17 +0100

    added Testfile for Controller

---
+++ backend/src/test/java/org/example/backend/controller/SterneControllerTest.java
@@ -0,0 +1,24 @@
+package org.example.backend.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SterneControllerTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void deletePlenumsTermin() {
+    }
+
+    @Test
+    void updatePlenumsTermin() {
+    }
+}
