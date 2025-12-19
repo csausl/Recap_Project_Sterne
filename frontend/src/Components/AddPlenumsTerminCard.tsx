d7493ca77f2ea55b126155334a74e900a3de0d98
chris s <test@test.com>
2025-12-19 12:02:42 +0100

    changed Date input to return Date type made Date required removed values from input removed default value from Group Select

--- frontend/src/Components/AddPlenumsTerminCard.tsx
+++ frontend/src/Components/AddPlenumsTerminCard.tsx
@@ -3,8 +3,8 @@ import {type PlenumsTerminDto, type Subgroup, subgroups} from "../Types/Types.ts
 import axios from "axios";
 
 export default function AddPlenumsTerminCard() {
-    const [plenumDate, setPlenumDate] = useState<string>("");
-    const [plenumGroup, setPlenumGroup] = useState<Subgroup>("ALLE");
+    const [plenumDate, setPlenumDate] = useState<Date>(new Date());
+    const [plenumGroup, setPlenumGroup] = useState<Subgroup>(undefined);
     const [plenumFirstTop, setPlenumFirstTop] = useState<string>("");
     const [plenumSecondTop, setPlenumSecondTop] = useState<string>("");
     const [plenumThirdTop, setPlenumThirdTop] = useState<string>("");
@@ -20,8 +20,8 @@ export default function AddPlenumsTerminCard() {
     };
 
     function resetForm() {
-        setPlenumDate("");
-        setPlenumGroup("ALLE");
+        setPlenumDate(new Date());
+        setPlenumGroup(undefined);
         setPlenumFirstTop("");
         setPlenumSecondTop("");
         setPlenumThirdTop("");
@@ -42,36 +42,38 @@ export default function AddPlenumsTerminCard() {
     return (
             <form className={"plenumsForm"} onSubmit={handleSubmit}>
                 <label>Date:<input
-                    name={"Date"}
-                    onChange={(e) => setPlenumDate(e.target.value)}
-                    placeholder={"Enter Date"}
-                    value={plenumDate}/>
+                    type="date"
+                    onChange={(e) => setPlenumDate((new Date(e.target.value)))}
+                    min="2025-01-01"
+                    max="2050-12-31"
+                    required={true}/>
                 </label>
                 <label>Top 1: <input
                     name={"Top 1"}
                     onChange={(e) => setPlenumFirstTop(e.target.value)}
                     placeholder={"Enter Top 1"}
-                    value={plenumFirstTop}/>
+                    />
                 </label>
                 <label>Top 2: <input
                     name={"Top 2"}
                     onChange={(e) => setPlenumSecondTop(e.target.value)}
                     placeholder={"Enter Top 2"}
-                    value={plenumSecondTop}/>
+                    />
                 </label>
                 <label>Top 3: <input
                     name={"Top 3"}
                     onChange={(e) => setPlenumThirdTop(e.target.value)}
                     placeholder={"Enter Top 3"}
-                    value={plenumThirdTop}/>
+                    />
                 </label>
                 <label>Gruppe:<select
-                    value={plenumGroup}
-                    //onChange={handleChange}>
                     onChange={handleChange}>
+                    required={true}
+                    <option value="">please choose</option>
                     {subgroups.map((category) => (
                         <option value={category.value} key={category.value}>{category.label}</option>
                     ))}
+
                 </select>
                 </label>
                 <button type="submit">Submit</button>
