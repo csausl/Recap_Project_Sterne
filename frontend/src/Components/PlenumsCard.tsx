b2b6f97235a7ff7f87972cb2b6eaa82995d746d9
chris s <test@test.com>
2026-01-08 13:20:14 +0100

    added deleting with confirm popup

--- frontend/src/Components/PlenumsCard.tsx
+++ frontend/src/Components/PlenumsCard.tsx
@@ -1,26 +1,34 @@
 import type {PlenumsTermin} from "../Types/Types.ts";
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 type PlenumsCardProps={
     plenum:PlenumsTermin
 }
 
-
-
 export default function PlenumsCard(props:Readonly<PlenumsCardProps>) {
+   const backToPlena = useNavigate();
+
     function deleteThisItem() {
         axios.delete("/api/plena/" + props.plenum.id)
             .then(res => {console.log(res.data)})
+            .then(() => {backToPlena("/plena")})
+    }
+
+    function confirmDelete(){
+        if(confirm("Are you sure you want to delete?")){
+            deleteThisItem();
+        }
     }
 
     return (
                 <div className={"plenumsCard"} key={props.plenum.id}>
-                    <p>group: {props.plenum.group}</p>
-                    <p>date: {props.plenum.date}</p>
+                    <p>{props.plenum.group}</p>
+                    <p>{props.plenum.date}</p>
                     <ul>Tops:
                         {props.plenum.tops.map((top,index) => (<li key={index}>{top}</li>))}
                     </ul>
-                    <button onClick={deleteThisItem}>Delete!</button>
+                    <button onClick={confirmDelete}>Delete!</button>
                 </div>
     )
 }
