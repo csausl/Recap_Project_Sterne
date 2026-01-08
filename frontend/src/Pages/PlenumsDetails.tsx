9eb10a44c5e3e3c139450bf0bb9152f17fa0b6cf
chris s <test@test.com>
2026-01-08 16:01:55 +0100

    added Navbar added update box

--- frontend/src/Pages/PlenumsDetails.tsx
+++ frontend/src/Pages/PlenumsDetails.tsx
@@ -2,8 +2,10 @@ import {useEffect, useState} from "react";
 
 import {type PlenumsTermin} from "../Types/Types.ts";
 import PlenumsCard from "../Components/PlenumsCard.tsx";
-import {Link, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import axios from "axios";
+import Navbar from "../Components/Navbar.tsx";
+import AddPlenumsTerminCard from "../Components/PlenumUpdateForm.tsx";
 
 export default function PlenumsDetails() {
     //const beispielTermin:PlenumsTermin={id:"1",date:"1.1.2011",group:"ALLE",tops:["","",""]};
@@ -23,18 +25,19 @@ export default function PlenumsDetails() {
 
     return (
         <>
-        <div className={"topBar"}>
-            <Link to={"/Plena"}><button className={"backbutton"}>Plena</button></Link>
-            <h1 className={"pageName"}></h1>
-            <button className={"logoutButton"}>logout</button>
-        </div>
+            <header className={"topBar"}><Navbar/></header>
         <div className={"mainContainer"}>
            <div className={"rightContainer"}>
                <div>
-                   {!plenum ? <div>loading..</div> : <PlenumsCard plenum={plenum} key={plenum.id}/>}
+                   {!plenum ? <div>loading..</div> : <PlenumsCard plenum={plenum}/>}
                </div>
             </div>
         </div>
+            <h1>Update:</h1>
+            <div className={"mainContainer"}>
+
+                {!plenum? <div>loading..</div> : <AddPlenumsTerminCard plenum={plenum} onUpdate={getPlenumById}/>}
+            </div>
         </>
     )
 }
