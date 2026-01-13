7d38a6218e13e41e0e94b125ffa60557acb3a56b
chris s <test@test.com>
2026-01-13 12:03:36 +0100

    styling

--- frontend/src/Pages/Infos.tsx
+++ frontend/src/Pages/Infos.tsx
@@ -57,7 +57,7 @@ export default function Infos() {
                     <label className="text-2xl font-semibold">Dein name</label>
                     <input
                         className="w-4/5 border rounded border-gray-400 text-lg leading-tight
-                         py-3 px-2 mt-4 mb-3 focus: outline-red-200"
+                         py-3 px-2 mt-4 mb-3 focus:outline-2 focus:outline-red-300"
                         id="firstName"
                         type="text"
                         name="firstName"
@@ -67,7 +67,7 @@ export default function Infos() {
                     <label className="text-2xl font-semibold">Email Adress</label>
                     <input
                         className="w-4/5 border border-gray-400 rounded text-lg leading-tight
-                         py-3 px-2 mt-4 mb-3 focus: outline-red-200"
+                         py-3 px-2 mt-4 mb-3 focus:outline-2 focus:outline-red-300"
                         id="email"
                         type="email"
                         name="email"
@@ -77,7 +77,7 @@ export default function Infos() {
                     <label className="text-2xl font-semibold">Datum</label>
                     <input
                         className="w-4/5 border border-gray-400 rounded text-lg leading-tight
-                         py-3 px-2 mt-4 mb-3 focus: outline-red-200"
+                         py-3 px-2 mt-4 mb-3 focus:outline-2 focus:outline-red-300"
                         id="date"
                         type="date"
                         name="date"
@@ -88,7 +88,7 @@ export default function Infos() {
                     <label className="text-2xl font-semibold">Gruppe</label>
                     <select
                         className="w-4/5 border border-gray-400 rounded text-lg leading-tight
-                         py-3 px-2 mt-4 mb-3 focus: outline-red-200"
+                         py-3 px-2 mt-4 mb-3 focus:outline-2 focus:outline-red-300"
                         id="group"
                         name="group"
                         onChange={handleInput}>
@@ -104,7 +104,7 @@ export default function Infos() {
                             <label className="text-2xl font-semibold">Infopunkt {i+1}</label>
                             <div className="flex flex-row items-center">
                             <textarea className="w-4/5 text-xl mb-4 border border-dashed border-gray-400 py-2 px-2
-                                        focus:outline-red-200"
+                                        focus:outline-2 focus:outline-red-300"
                                       id="top"
                                       name="top"
                                       rows={2}
