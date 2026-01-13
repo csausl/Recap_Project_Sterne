ce63278df1ee9f2db6259eaa64c7c8605513d0da
chris s <test@test.com>
2026-01-13 13:26:36 +0100

    added new Form removed old form
    changed input handling to a single on change function for details and tops
    date handling changed from ISO String to just String (YYYY-MM-DD) - compatible with displaying and ordering the dates
    made styling classes to reduce tailwindcss
    removed redundant use states
    added items to Details and Tops (not submitted yet)

--- frontend/src/Components/AddPlenumsTerminCard.tsx
+++ frontend/src/Components/AddPlenumsTerminCard.tsx
@@ -1,81 +1,72 @@
-import {type ChangeEvent, type FormEvent, useEffect, useState} from "react";
+import {type FormEvent, useEffect, useState} from "react";
 import {type PlenumsTerminDto, type Subgroup, subgroups} from "../Types/Types.ts";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
 
-export type TerminData={
-    date: Date,
-    group: Subgroup
-    top: string
-}
-
 export default function AddPlenumsTerminCard() {
-    const [userInfo, setUserInfo] = useState({
+    const [plenumsInfo, setplenumsInfo] = useState({
         firstName: "",
         orga: "",
         date: "",
         group: "",
     })
-    const [tops, setTops] = useState([{
-        top: "",
+    const [topsForm, setTops] = useState([{
+        topContent: "",
+        topOrga: "",
         timestamp: new Date().getTime(),
     }])
 
-    console.log(userInfo);
-    console.log(tops);
-
-    const handleInput = (e) => {
-        const {name, value} = e.target;
-        setUserInfo({
-            ...userInfo,
+    const handleInput = (event) => {
+        const {name, value} = event.target;
+        setplenumsInfo({
+            ...plenumsInfo,
             [name]: value
         })
     }
 
-    const handleTop = (e, i: number) => {
-        console.log(i);
-        const {name, value} = e.target;
-        const newTops = [...tops];
-        newTops[i][name] = value;
+    const handleTop = (event, index: number) => {
+        const {name, value} = event.target;
+        const newTops = [...topsForm];
+        newTops[index][name] = value;
         setTops(newTops);
     }
 
     const handleRemoveTop = (i: number) => {
-        const deleteTops=[...tops];
+        const deleteTops=[...topsForm];
         deleteTops.splice(i,1);
         setTops(deleteTops);
     }
 
     const handleAddTop = () => {
-        setTops([...tops, {
-            top: "",
+        setTops([...topsForm, {
+            topContent: "",
+            topOrga:"",
             timestamp: new Date().getTime(),
         }])
     }
 
-    const [plenumDateString, setPlenumDateString] = useState<string>("");
-    const [plenumGroup, setPlenumGroup] = useState<Subgroup>(undefined);
-    const [plenumFirstTop, setPlenumFirstTop] = useState<string>("");
-    const [plenumSecondTop, setPlenumSecondTop] = useState<string>("");
-    const [plenumThirdTop, setPlenumThirdTop] = useState<string>("");
     const [plenumsTerminDto, setPlenumsTerminDto] = useState<PlenumsTerminDto>()
     const navigate = useNavigate();
 
     function handleSubmit(event:FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        setPlenumsTerminDto({date: plenumDateString, group: plenumGroup, tops: [plenumFirstTop,plenumSecondTop,plenumThirdTop]});
+        const topArray=topsForm.map(a => a.topContent);
+        setPlenumsTerminDto({date: plenumsInfo.date, group: plenumsInfo.group as Subgroup, tops: topArray});
     }
 
-    const handleChange = (event:ChangeEvent<HTMLSelectElement>) => {
-        setPlenumGroup(event.target.value as Subgroup);
-    };
-
     function resetForm() {
-        setPlenumDateString("")
-        setPlenumGroup("");
-        setPlenumFirstTop("");
-        setPlenumSecondTop("");
-        setPlenumThirdTop("");
+        setplenumsInfo({
+            firstName: "",
+            orga: "",
+            date: "",
+            group: "",
+        })
+
+        setTops([{
+            topContent: "",
+            topOrga: "",
+            timestamp: new Date().getTime(),
+        }])
     }
 
     function addNewPlenumstermin(){
@@ -97,64 +88,19 @@ export default function AddPlenumsTerminCard() {
 
     }, [plenumsTerminDto]);
 
+    console.log(plenumsInfo);
+    console.log(topsForm);
+
+
     return (
         <>
-            <form className={"plenumsForm"} onSubmit={handleSubmit}>
-                <label>Date:<input
-                    //value={plenumDate}
-                    type="date"
-                    name="plenumDate"
-                    onChange={(e) =>{
-                        const dateString = (new Date(e.target.value)).toISOString();
-                        setPlenumDateString(dateString);}
-                    }
-                    min="2025-01-01"
-                    max="2050-12-31"
-                    required={true}/>
-                </label>
-                <label>Top 1: <input
-                    value={plenumFirstTop}
-                    name={"plenumFirstTop"}
-                    onChange={(e) => setPlenumFirstTop(e.target.value)}
-                    placeholder={"Enter Top 1"}
-                    />
-                </label>
-                <label>Top 2: <input
-                    value={plenumSecondTop}
-                    name={"plenumSecondTop"}
-                    onChange={(e) => setPlenumSecondTop(e.target.value)}
-                    placeholder={"Enter Top 2"}
-                    />
-                </label>
-                <label>Top 3: <input
-                    value={plenumThirdTop}
-                    name={"plenumThirdTop"}
-                    onChange={(e) => setPlenumThirdTop(e.target.value)}
-                    placeholder={"Enter Top 3"}
-                    />
-                </label>
-                <label>Gruppe:<select
-                    value={plenumGroup}
-                    required={true}
-                    onChange={handleChange}
-                    name="plenumGroup">
-                    <option value="">please choose</option>
-                    {subgroups.map((category) => (
-                        <option value={category.value} key={category.value}>{category.label}</option>
-                    ))}
-
-                </select>
-                </label>
-                <button type="submit">Submit</button>
-                <button type="reset" onClick={resetForm}>Reset</button>
-            </form>
-
-            <form className="formLayout">
+            <label className="text-4xl font-semibold mt-5 mb-5">Plenumstermin hinzufügen</label>
+            <form className="formLayout" onSubmit={handleSubmit}>
                 <fieldset className="formFieldSet">
-                    <legend className="formLegend">Information Hinzufügen</legend>
+                    <legend className="formLegend">Termin Details</legend>
                     <label className="formLabel">Dein Name</label>
                     <input
-                        className="formInput focus:outline-2 focus:outline-red-300"
+                        className="formInput "
                         id="firstName"
                         type="text"
                         name="firstName"
@@ -163,7 +109,7 @@ export default function AddPlenumsTerminCard() {
                     />
                     <label className="formLabel">Orga</label>
                     <input
-                        className="formInput focus:outline-2 focus:outline-red-300"
+                        className="formInput "
                         id="orga"
                         type="text"
                         name="orga"
@@ -172,8 +118,7 @@ export default function AddPlenumsTerminCard() {
                     />
                     <label className="formLabel">Datum</label>
                     <input
-                        className="formInput focus:outline-2 focus:outline-red-300"
-                        id="date"
+                        className="formInput"
                         type="date"
                         name="date"
                         onChange={handleInput}
@@ -182,7 +127,7 @@ export default function AddPlenumsTerminCard() {
                     />
                     <label className="formLabel">Gruppe</label>
                     <select
-                        className="formInput focus:outline-2 focus:outline-red-300"
+                        className="formInput "
                         id="group"
                         name="group"
                         onChange={handleInput}>
@@ -192,21 +137,29 @@ export default function AddPlenumsTerminCard() {
                     </select>
                 </fieldset>
                 <fieldset className="formFieldSet">
-                    <legend className="formLegend">Tagesordnungpunkte</legend>
-                    {tops.map((top, i) => (
+                    <legend className="formLegend">Tagesordnungspunkte</legend>
+                    {topsForm.map((top, index) => (
                         <div key={top.timestamp} className="flex flex-col">
-                            <label className="formLabel">TOP {i+1}</label>
+                            <label className="formLabel">TOP {index+1}</label>
+                            <input
+                                className="formInput max-w-2/4 "
+                                id="topOrga"
+                                type="text"
+                                name="topOrga"
+                                placeholder="Von wem?"
+                                onChange={event => handleTop(event, index)}
+                            />
                             <div className="flex flex-row items-center">
-                            <textarea className="formInput focus:outline-2 focus:outline-red-300"
-                                      id="top"
-                                      name="top"
+                            <textarea className="formInput "
+                                      id="topContent"
+                                      name="topContent"
                                       rows={2}
-                                      placeholder="Was ist zu tun?"
-                                      onChange={e => handleTop(e, i)}/>
+                                      placeholder="Was ist zu besprechen?"
+                                      onChange={event => handleTop(event, index)}/>
                                 <button
                                     className="w-1/5 border border-gray-400 text-lg leading-tight"
                                     type={"button"}
-                                    onClick={()=> handleRemoveTop(i)}
+                                    onClick={()=> handleRemoveTop(index)}
                                 > - </button>
                             </div>
                         </div>
@@ -224,6 +177,7 @@ export default function AddPlenumsTerminCard() {
 
                 </fieldset>
                 <button type="submit">Submit</button>
+                <button type="reset" onClick={resetForm}>Reset</button>
             </form>
         </>
     )
