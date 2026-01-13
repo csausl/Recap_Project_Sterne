be60b186c8fb1db6ed1f7f023044aee43c5d6984
chris s <test@test.com>
2026-01-13 12:05:22 +0100

    removed test code

--- frontend/src/Pages/LandingPage.tsx
+++ frontend/src/Pages/LandingPage.tsx
@@ -1,34 +1,5 @@
 import {Link} from "react-router-dom";
-function myFunction() {
-    alert("whooohooo!");
-}
 
-function myFunction2() {
-    let txt;
-    if (confirm("Press a button!")) {
-        txt = "<button onClick={myFunction} >You pressed OK!</button>";
-    } else {
-        txt = "You pressed Cancel!";
-    }
-    if(document != null){
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error
-        document.getElementById("demo").innerHTML = txt;
-    }
-
-}
-function myFunction4() {
-    const x = document.getElementById("myDIV");
-    if(x!=null){
-        if (x.style.display === "block") {
-            x.style.display = "none";
-        } else {
-            x.style.display = "block";
-        }
-    }
-
-
-}
 export default function LandingPage(){
 
 
@@ -36,8 +7,6 @@ export default function LandingPage(){
     return (
         <>
             <header className={"topBar"} ><h1>Du bist mein Stern</h1></header>
-
-
             <ul className="flex-container">
                 <Link to={"/Infos"}>
                     <li className="categoryCard">Infos</li>
