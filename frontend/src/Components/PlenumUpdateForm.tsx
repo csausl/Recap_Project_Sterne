ffd1541b598bea1119bf0adb263581eb6a72da07
chris s <test@test.com>
2026-01-13 17:39:46 +0100

    moved to new form added default states for form

--- frontend/src/Components/PlenumUpdateForm.tsx
+++ frontend/src/Components/PlenumUpdateForm.tsx
@@ -9,11 +9,107 @@ type UpdateProps={
 }
 
 export default function PlenumUpdateForm(props:Readonly<UpdateProps>) {
-    const [plenumDateString, setPlenumDateString] = useState<string>(props.plenum.date);
-    const [plenumGroup, setPlenumGroup] = useState<Subgroup>(props.plenum.group);
-    const [plenumFirstTop, setPlenumFirstTop] = useState<string>(props.plenum.tops[0]);
-    const [plenumSecondTop, setPlenumSecondTop] = useState<string>(props.plenum.tops[1]);
-    const [plenumThirdTop, setPlenumThirdTop] = useState<string>(props.plenum.tops[2]);
+    const [plenumsInfo, setplenumsInfo] = useState({
+        firstName: "",
+        orga: "",
+        date: "",
+        group: "",
+    })
+    const [topsForm, setTops] = useState([{
+        topContent: "",
+        topOrga: "",
+        timestamp: 0,
+    }])
+
+    if (plenumsInfo.date === "") {
+        console.log(props.plenum,plenumsInfo)
+        setplenumsInfo({
+            firstName: "",
+            orga: "",
+            date: props.plenum.date,
+            group: props.plenum.group as string,
+        })
+        console.log(plenumsInfo)
+
+        props.plenum.tops.map((top, index) => {
+
+            topsForm.push({
+                topContent: top,
+                topOrga: "",
+                timestamp: index,
+            });
+            const deleteTops=[...topsForm];
+            deleteTops.splice(0,1);
+            setTops(deleteTops);
+        })
+
+    }
+
+    const handleInput = (event) => {
+        const {name, value} = event.target;
+        setplenumsInfo({
+            ...plenumsInfo,
+            [name]: value
+        })
+    }
+
+    const handleTop = (event, index: number) => {
+        const {name, value} = event.target;
+        const newTops = [...topsForm];
+        newTops[index][name] = value;
+        setTops(newTops);
+    }
+
+    const handleRemoveTop = (i: number) => {
+        const deleteTops=[...topsForm];
+        deleteTops.splice(i,1);
+        setTops(deleteTops);
+    }
+
+    const handleAddTop = () => {
+        setTops([...topsForm, {
+            topContent: "",
+            topOrga:"",
+            timestamp: new Date().getTime(),
+        }])
+    }
+
+    function handleSubmitNew(event:FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const topArray=topsForm.map(a => a.topContent);
+        setUpdatedPlenumsTerminDto({date: plenumsInfo.date, group: plenumsInfo.group as Subgroup, tops: topArray});
+        //setPlenumsTerminDto({date: plenumsInfo.date, group: plenumsInfo.group as Subgroup, tops: topArray});
+    }
+
+    function resetFormNew() {
+        console.log(props.plenum,plenumsInfo)
+        setplenumsInfo({
+            firstName: "",
+            orga: "",
+            date: props.plenum.date,
+            group: props.plenum.group as string,
+        })
+        console.log(plenumsInfo)
+
+        setTops([{
+            topContent: "",
+            topOrga: "",
+            timestamp: 0,
+        }])
+
+        props.plenum.tops.map((top, index) => {
+
+            topsForm.push({
+                topContent: top,
+                topOrga: "",
+                timestamp: index,
+            });
+            const deleteTops=[...topsForm];
+            deleteTops.splice(0,1);
+            setTops(deleteTops);
+        })
+    }
+
     const [updatedPlenumsTerminDto, setUpdatedPlenumsTerminDto] = useState<PlenumsTerminDto>();
 
     const backToPlena = useNavigate();
@@ -25,28 +121,11 @@ export default function PlenumUpdateForm(props:Readonly<UpdateProps>) {
     }
 
     function confirmUpdate(event:FormEvent<HTMLFormElement>){
-        if(confirm("Are you sure you want to update?")){
-            handleSubmit(event);
+        if(confirm("update OK?")){
+            handleSubmitNew(event);
         }
+        else event.preventDefault();
     }
-
-    function handleSubmit(event:FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        setUpdatedPlenumsTerminDto({date: plenumDateString, group: plenumGroup, tops: [plenumFirstTop,plenumSecondTop,plenumThirdTop]});
-    }
-
-    const handleChange = (event:ChangeEvent<HTMLSelectElement>) => {
-        setPlenumGroup(event.target.value as Subgroup);
-    };
-
-    function resetForm() {
-        setPlenumDateString(props.plenum.date)
-        setPlenumGroup(props.plenum.group);
-        setPlenumFirstTop(props.plenum.tops[0]);
-        setPlenumSecondTop(props.plenum.tops[1]);
-        setPlenumThirdTop(props.plenum.tops[2]);
-    }
-
     useEffect(() => {
         if(updatedPlenumsTerminDto){
             updatePlenumsTermin();
@@ -54,53 +133,98 @@ export default function PlenumUpdateForm(props:Readonly<UpdateProps>) {
         }
     }, [updatedPlenumsTerminDto]);
 
+
     return (
-        <form className={"plenumsForm"} onSubmit={confirmUpdate}>
-            <label>Date:<input
-                //value={plenumDate}
-                //value="2026-01-01"
-                type="date"
-                onChange={(e) =>{
-                    const dateString = (new Date(e.target.value)).toISOString();
-                    setPlenumDateString(dateString);}
-                }
-                min="2025-01-01"
-                max="2050-12-31"
-                required={true}/>
-            </label>
-            <label>Top 1: <input
-                value={plenumFirstTop}
-                name={"Top 1"}
-                onChange={(e) => setPlenumFirstTop(e.target.value)}
-                placeholder={props.plenum.tops[0]}
-            />
-            </label>
-            <label>Top 2: <input
-                value={plenumSecondTop}
-                name={"Top 2"}
-                onChange={(e) => setPlenumSecondTop(e.target.value)}
-                placeholder={props.plenum.tops[1]}
-            />
-            </label>
-            <label>Top 3: <input
-                value={plenumThirdTop}
-                name={"Top 3"}
-                onChange={(e) => setPlenumThirdTop(e.target.value)}
-                placeholder={props.plenum.tops[2]}
-            />
-            </label>
-            <label>Gruppe:<select
-                value={plenumGroup}
-                required={true}
-                onChange={handleChange}>
-                <option value="">bitte auswählen</option>
-                {subgroups.map((category) => (
-                    <option value={category.value} key={category.value}>{category.label}</option>
-                ))}
-            </select>
-            </label>
-            <button type="submit">Submit</button>
-            <button type="reset" onClick={resetForm}>Reset</button>
-        </form>
+        <>
+            <form className="formLayout" onSubmit={confirmUpdate}>
+                <fieldset className="formFieldSet">
+                    <legend className="formLegend">Termin Details</legend>
+                    <label className="formLabel">Dein Name</label>
+                    <input
+                        className="formInput "
+                        id="firstName"
+                        type="text"
+                        name="firstName"
+                        placeholder="Name"
+                        onChange={handleInput}
+                    />
+                    <label className="formLabel">Orga</label>
+                    <input
+                        className="formInput "
+                        id="orga"
+                        type="text"
+                        name="orga"
+                        placeholder="Wer macht die Orga?"
+                        onChange={handleInput}
+                    />
+                    <label className="formLabel">Datum</label>
+                    <input
+                        className="formInput"
+                        type="date"
+                        name="date"
+                        required={true}
+                        onChange={handleInput}
+                        min="2025-01-01"
+                        max="2050-12-31"
+                    />
+                    <label className="formLabel">Gruppe</label>
+                    <select
+                        className="formInput "
+                        id="group"
+                        name="group"
+                        required={true}
+                        value={plenumsInfo.group}
+                        onChange={handleInput}>
+                        {subgroups.map((category) => (
+                            <option value={category.value} key={category.value}>{category.label}</option>
+                        ))}
+                    </select>
+                </fieldset>
+                <fieldset className="formFieldSet">
+                    <legend className="formLegend">Tagesordnungspunkte</legend>
+                    {topsForm.map((top, index) => (
+                        <div key={top.timestamp} className="flex flex-col">
+                            <label className="formLabel">TOP {index+1}</label>
+                            <input
+                                className="formInput max-w-2/4 "
+                                id="topOrga"
+                                type="text"
+                                name="topOrga"
+                                value=""
+                                placeholder="Von wem?"
+                                onChange={event => handleTop(event, index)}
+                            />
+                            <div className="flex flex-row items-center">
+                            <textarea className="formInput "
+                                      id="topContent"
+                                      name="topContent"
+                                      rows={2}
+                                      placeholder={top.topContent}
+                                      value={top.topContent}
+                                      onChange={event => handleTop(event, index)}/>
+                                <button
+                                    className="w-1/5 border border-gray-400 text-lg leading-tight"
+                                    type={"button"}
+                                    onClick={()=> handleRemoveTop(index)}
+                                > - </button>
+                            </div>
+                        </div>
+                    ))}
+                    <div className="w-full flex justify-center">
+                        <button
+                            className="w-1/5 border border-gray-400 text-lg leading-tight"
+                            type="button"
+                            onClick={handleAddTop}
+                        >
+                            +
+                        </button>
+                    </div>
+
+
+                </fieldset>
+                <button type="submit">Submit</button>
+                <button type="reset" onClick={resetFormNew}>Reset</button>
+            </form>
+        </>
     )
 }
